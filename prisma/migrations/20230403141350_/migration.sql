033397f7668f4c17c9dce69994b20e6c989d06c2
Ismael Junior <ismaeljcjunior@gmail.com>
2023-04-13 08:39:08 -0300

    FEAT

--- prisma/migrations/20230403141350_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `IP_EEQUIPAMEMENT` to the `destination_equipment` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `destination_equipment` ADD COLUMN `IP_EEQUIPAMEMENT` VARCHAR(191) NOT NULL;
--- prisma/migrations/20230403141810_/migration.sql
+++
@@ -1,2 +0,0 @@
--- DropIndex
-DROP INDEX `destination_equipment_ID_DESTINATIONS_key` ON `destination_equipment`;
